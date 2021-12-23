4407b78f036da04d0c544b4d699651352aa3a148
Your Name <lucas.petigato@hotmail.com>
2021-12-23 16:23:49 -0400

    primeiro commit

---
+++ testeAlgoritmos.js
@@ -0,0 +1,99 @@
+// q2
+
+const inverteArray = entrada => {
+    let invertido = [];
+
+    for (let i = entrada.length - 1; i>=0; i--) {
+        invertido.push(entrada[i]);
+    }
+    return invertido;
+};
+
+inverteArray([1,2,3,4]);
+
+// q3
+
+const limpaArray = entrada => {
+    let novoArray = [];
+
+    for (let i = 0; i < entrada.length; i++) {
+        if (entrada[i]) {
+            novoArray.push(entrada[i]);
+        }
+    }
+
+    return novoArray;
+};
+
+limpaArray([1, 2, "", undefined]);
+
+// q5
+
+const removeIgualParametro = (entrada, parametro1, parametro2) => {
+    let novoArray = [];
+
+    for (let i = 0; i < entrada.length; i++) {
+        if (entrada[i] != parametro1 && parametro2){
+            novoArray.push(entrada[i]);
+        }
+    }
+    return novoArray;
+};
+
+console.log(removeIgualParametro([5,4,3,2,5], 5, 3));
+
+// q6 - TERMINAR, TÁ ERRADA
+
+const removeDuplicado = entrada => {
+    let novoArray = [];
+
+    for (let i = 0; i < entrada.length; i++) {
+        for (let j = entrada.length; j > 0; j--) {
+            if (entrada[i] !== entrada[j]) {
+                novoArray.push(entrada[i]);
+            }
+        }
+    }
+
+    return novoArray;
+}
+
+removeDuplicado([1,2,3,3,2,4,5,4,7,3]);
+
+// q7
+
+const comparaArrays = (entrada1, entrada2) => {
+    let cont = 0;
+
+    for (let i = 0; i < entrada1.length; i++) {
+        for (let j = 0; j < entrada2.length; j++) {
+            if (entrada1[i] == entrada2[j]){
+                cont++;
+            }
+        }
+    }
+
+    if (cont === entrada1.length && entrada2.length) {
+        return true;
+    }
+    return false;
+};
+
+comparaArrays([1,2,3,4], [1,2,3,4]);
+
+// q10
+
+const valorComum = (entrada1, entrada2) => {
+    let novoArray = [];
+
+    for (let i = 0; i < entrada1.length; i++) {
+        for (let j = 0; j < entrada2.length; j++) {
+            if (entrada1[i] == entrada2[j]){
+                novoArray.push(entrada1[i]);
+            }
+        }
+    }
+    return novoArray;
+};
+
+valorComum([6,8], [6, 8]);
